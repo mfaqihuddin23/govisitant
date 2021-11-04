bffedab29178e062331c6c9154e0fe9771a5bcb4
iMac27 <imac27@iMac27s-iMac-2.local>
2021-11-04 15:24:09 +0700

    adding animation on section 1

--- assets/js/sectiontrans.js
+++ assets/js/sectiontrans.js
@@ -2,16 +2,57 @@ let sections = document.getElementsByTagName('section');
 // tracks index of current section
 let currentSectionIndex = 0;
 
+element = document.getElementById("animtitle");
+element.addEventListener("click", function(e) {
+  e.preventDefault;
+  
+  // -> removing the class
+  element.classList.remove("animrun");
+  
+  // -> triggering reflow /* The actual magic */
+  // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
+  // Oops! This won't work in strict mode. Thanks Felis Phasma!
+  // element.offsetWidth = element.offsetWidth;
+  // Do this instead:
+  void element.offsetWidth;
+  
+  // -> and re-adding the class
+  element.classList.add("animrun");
+}, false);
+
 document.addEventListener('wheel', e => {
   if (e.wheelDeltaY > 0 && currentSectionIndex - 1 >= 0) {
     // wheel up
     sections[currentSectionIndex].className = '';
     currentSectionIndex--;
     sections[currentSectionIndex].className = 'active';
+    
+    element.classList.remove("animrun");
+  
+    // -> triggering reflow /* The actual magic */
+    // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
+    // Oops! This won't work in strict mode. Thanks Felis Phasma!
+    // element.offsetWidth = element.offsetWidth;
+    // Do this instead:
+    void element.offsetWidth;
+    
+    // -> and re-adding the class
+    element.classList.add("animrun");
   } else if (e.wheelDeltaY < 0 && currentSectionIndex + 1 < sections.length) {
     // wheel down
     sections[currentSectionIndex].className = '';
     currentSectionIndex++;
     sections[currentSectionIndex].className = 'active';
+
+    element.classList.remove("animrun");
+    // -> triggering reflow /* The actual magic */
+    // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
+    // Oops! This won't work in strict mode. Thanks Felis Phasma!
+    // element.offsetWidth = element.offsetWidth;
+    // Do this instead:
+    void element.offsetWidth;
+    
+    // -> and re-adding the class
+    element.classList.add("animrun");
   }
 });
